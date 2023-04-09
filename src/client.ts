eb165d18b555141b87ae2c4d9e5253f921642f72
Paul Ridgway <paul@ridgway.io>
2023-04-09 10:38:07 +0100

    fix limit

--- src/client.ts
+++ src/client.ts
@@ -84,7 +84,7 @@ export class Client {
     return (await this._client.get<InverterCountApiResponse>(`/api/v1/plant/${plantId}/inverterCount`)).data.data;
   }
 
-  async getInverters(plantId: number, page: number = 1, limit: number = 1, status: number = -1, sn: string = '', type: number = -2) {
+  async getInverters(plantId: number, page: number = 1, limit: number = 10, status: number = -1, sn: string = '', type: number = -2) {
     return (await this._client.get<InvertersApiResponse>(`/api/v1/plant/${plantId}/inverters?page=${page}&limit=${limit}&status=${status}&sn=${sn}&id=${plantId}type=${type}`)).data.data;
   }
 
